1a991c47af453071cbdbbfda9ee1b06b2afc7d56
Leevenjun <i_suichuan@163.com>
2019-04-17 15:10:39 +0800

    更新globalparam.py的的全局项目设置，摆脱config配置文件限制

--- config/globalparam.py
+++ config/globalparam.py
@@ -18,23 +18,14 @@ log_path = os.path.join(prj_path, 'report', 'log')
 img_path = os.path.join(prj_path, 'report', 'image')
 # Exception screenshot file path
 eximg_path = os.path.join(prj_path, 'report', 'exception_img')
-print(eximg_path)
 # 测试报告文件路径
 report_path = os.path.join(prj_path, 'report', 'test_report')
 # Upload the autoit file path
 auto_path = os.path.join(prj_path, 'up_files', 'autoit_pic')
 # yaml配置文件路径
 yaml_path = os.path.join(prj_path, 'config', 'readyaml')
-print(yaml_path)
 # Default browser
 browser = 'chrome'
 
 # 测试数据路径
 data_path = os.path.join(prj_path, '', 'testdata')
-
-project_path = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
-# print(project_path)  # 获取当前项目路径
-y_path = os.path.join(project_path, 'config', 'db.yaml')
-# print(y_path)    # 获取db.yaml文件路径
-print(project_path)
-print(y_path)
