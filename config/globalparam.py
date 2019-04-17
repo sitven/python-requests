51fc5c7ba62b65d60ff4f97f2d7c8ee026363aca
Leevenjun <i_suichuan@163.com>
2019-04-17 15:08:16 +0800

    更新globalparam.py的的全局项目设置，摆脱config配置文件限制

--- config/globalparam.py
+++ config/globalparam.py
@@ -3,31 +3,38 @@ import os
 
 from branch.readconfig import ReadConfig
 
-# Read configuration file
-config_file_path = os.path.split(os.path.realpath(__file__))[0]
-read_config = ReadConfig(os.path.join(config_file_path, 'config.ini'))
-# Project parameter setting
-prj_path = read_config.getValue('projectConfig', 'project_path')
-# print(prj_path)
+# 项目路径参数设置
+prj_path = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
+
+# config配置文件路径
+config_file_path = os.path.join(prj_path, "config")
+read_config = ReadConfig(os.path.join(config_file_path, 'config.ini'))      # 读取
+
 # Log path
 log_path = os.path.join(prj_path, 'report', 'log')
-# Screenshot file path
+# print(log_path)
+
+# 截图路径
 img_path = os.path.join(prj_path, 'report', 'image')
-#Exception screenshot file path
+# Exception screenshot file path
 eximg_path = os.path.join(prj_path, 'report', 'exception_img')
-#Test report path
+print(eximg_path)
+# 测试报告文件路径
 report_path = os.path.join(prj_path, 'report', 'test_report')
-#Upload the autoit file path
+# Upload the autoit file path
 auto_path = os.path.join(prj_path, 'up_files', 'autoit_pic')
-#
+# yaml配置文件路径
 yaml_path = os.path.join(prj_path, 'config', 'readyaml')
+print(yaml_path)
 # Default browser
 browser = 'chrome'
-# Test data path
-data_path = os.path.join(prj_path, '', 'testdata')
 
+# 测试数据路径
+data_path = os.path.join(prj_path, '', 'testdata')
 
 project_path = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 # print(project_path)  # 获取当前项目路径
 y_path = os.path.join(project_path, 'config', 'db.yaml')
 # print(y_path)    # 获取db.yaml文件路径
+print(project_path)
+print(y_path)
